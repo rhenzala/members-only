1e65a4e60d546cde34ffebfb6166f735acc6a0c5
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-03-09 18:56:13 +0800

    Separate the routing and authentication

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require("node:path");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const appRouter = require("./routes/appRouter");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", appRouter);
+
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
+
+
+
+ 
+
+
+
+
+
+
